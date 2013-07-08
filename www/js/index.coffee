2650e93aa28ce805a053174a9b36876281b62b64
Anshula Gandhi <iceskatr@gmail.com>
2013-07-08 18:57:52 -0400

    added in event for submit button (which logs to the console)

--- www/js/index.coffee
+++ www/js/index.coffee
@@ -3,10 +3,10 @@ $ ->
 	
 	class window.MyRouter extends Backbone.Router
 		routes:
-			"": "index"
-			"steps": "showSteps"
+			"": "showHome"
+			"safety": "showSafety"
 	
-		index: ->
+		showHome: ->
 			#log
 			console.log "show index.."
 			
@@ -18,7 +18,7 @@ $ ->
 			#change header
 			$('[data-role="header"] > h3').html "Home"	
 	
-		showSteps: ->
+		showSafety: ->
 			#log
 			console.log "show steps"
 			
@@ -27,24 +27,28 @@ $ ->
 			"""
 					
 			#change header
-			$('[data-role="header"] > h3').html "Steps"	
+			$('[data-role="header"] > h3').html "Safety Planning"	
 			
 			#change content
 			@list = new StepList
 			@list.add new Step 
-				title: 'Step 1: Warning Signs'
+				step_num: 1
+				title: 'Warning Signs'
 				description: 'Warning signs (thoughts, images, mood, situation, behavior) that a crisis may be developing:'
 				fields: ['warning sign']
 			@list.add new Step 
-				title: 'Step 2: Coping Strategies'
+				step_num: 2
+				title: 'Coping Strategies'
 				description: 'Internal coping strategies – things I can do to take my mind off my problems without contacting another person (relaxation technique, physical activity):'
 				fields: ['coping strategy']
 			@list.add new Step 
-				title: 'Step 3: People'
+				step_num: 3
+				title: 'People'
 				description: 'People that provide distraction:'
 				fields: ['name', 'phone number']
 			@list.add new Step 
-				title: 'Step 4: Settings'
+				step_num: 4
+				title: 'Settings'
 				description: 'Social settings that provide distraction:'
 				fields: ['place']
 			@listview = new StepListView {collection: @list}
@@ -90,16 +94,19 @@ $ ->
 
 
 	class window.StepView extends Backbone.View
+		
 		tagName: 'div'
 		attributes:
 			'data-role': 'collapsible'
 			'data-collapsed': 'true'
+		events:
+			"click .submit": "clicked"
 		initialize: ->
 			_.bindAll @
 			temp = """
 
 			<h3>
-			<%= title %> 
+			<%= step_num %> : <%= title %>
 			</h3>
 
 			<div style="font-size: 14px; color: #333; background-color: pink; padding: 10px; border-radius: 10px"> 
@@ -112,7 +119,7 @@ $ ->
 				<input name="" class="textinput" placeholder="Add <%= field %>" value="" type="text" data-mini="false" />
 			<% }); %>
 
-			<input type="submit" value="Submit" />
+			<input id="<%= step_num %>" class="submit" type="submit" value="Submit" />
 
 
 
@@ -129,27 +136,46 @@ $ ->
 			content = @template(@model.toJSON())
 			$(@el).html content
 			@ #return itself
+		
+		clicked: (e) =>
+			e.preventDefault()
+			output = []
+			$(@el).find('.textinput').each ->
+				output.push $(this).val()
+			#@model.addStrategy output
+			#console.log @model.get('strategies').toString()
 
 	class window.Step extends Backbone.Model
 		defaults:
-			title: 'Step 0'
+			step_num: 0
+			title: ''
 			description: 'Write some strategies.'
 			fields: []
 			strategies: []
+		# addStrategy: (output_array) ->
+		# 	@save
+		# 		strategies: @get('strategies').push output_array
 
-	new MyRouter
-	Backbone.history.start()
-	$(document).on "click", "a:not([data-bypass])", (evt) ->
-		href =
-			prop: $(this).prop("href")
-			attr: $(this).attr("href")
+	class window.App extends Backbone.Model
+		initialize: ->	
+			new MyRouter
+			Backbone.history.start()
+			@initMenu()
+			
+		initMenu: ->
+			#whenever a link in the menu is clicked
+			#it tells the backbone router to navigate there
+			$(document).on "click", "a:not([data-bypass])", (evt) ->
+				href =
+					prop: $(this).prop("href")
+					attr: $(this).attr("href")
 
-		root = location.protocol + "//" + location.host + "/"
-		if href.prop and href.prop.slice(0, root.length) is root
-			evt.preventDefault()
-			Backbone.history.navigate href.attr, true
+				root = location.protocol + "//" + location.host + "/"
+				if href.prop and href.prop.slice(0, root.length) is root
+					evt.preventDefault()
+					Backbone.history.navigate href.attr, true
 		
-		#close the panel
-		$('#myPanel').panel("close")
+				#close the panel
+				$('#myPanel').panel("close")
 
-	
+	app = new App
--- www/js/index.js
+++ www/js/index.js
@@ -5,7 +5,7 @@
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   $(function() {
-    var _ref, _ref1, _ref2, _ref3, _ref4;
+    var app, _ref, _ref1, _ref2, _ref3, _ref4, _ref5;
     console.log("ready");
     window.MyRouter = (function(_super) {
       __extends(MyRouter, _super);
@@ -16,39 +16,43 @@
       }
 
       MyRouter.prototype.routes = {
-        "": "index",
-        "steps": "showSteps"
+        "": "showHome",
+        "safety": "showSafety"
       };
 
-      MyRouter.prototype.index = function() {
+      MyRouter.prototype.showHome = function() {
         console.log("show index..");
         $('[data-role="content"]').html("Home page.");
         return $('[data-role="header"] > h3').html("Home");
       };
 
-      MyRouter.prototype.showSteps = function() {
+      MyRouter.prototype.showSafety = function() {
         var element;
         console.log("show steps");
         $('[data-role="content"]').html("");
-        $('[data-role="header"] > h3').html("Steps");
+        $('[data-role="header"] > h3').html("Safety Planning");
         this.list = new StepList;
         this.list.add(new Step({
-          title: 'Step 1: Warning Signs',
+          step_num: 1,
+          title: 'Warning Signs',
           description: 'Warning signs (thoughts, images, mood, situation, behavior) that a crisis may be developing:',
           fields: ['warning sign']
         }));
         this.list.add(new Step({
-          title: 'Step 2: Coping Strategies',
+          step_num: 2,
+          title: 'Coping Strategies',
           description: 'Internal coping strategies – things I can do to take my mind off my problems without contacting another person (relaxation technique, physical activity):',
           fields: ['coping strategy']
         }));
         this.list.add(new Step({
-          title: 'Step 3: People',
+          step_num: 3,
+          title: 'People',
           description: 'People that provide distraction:',
           fields: ['name', 'phone number']
         }));
         this.list.add(new Step({
-          title: 'Step 4: Settings',
+          step_num: 4,
+          title: 'Settings',
           description: 'Social settings that provide distraction:',
           fields: ['place']
         }));
@@ -118,6 +122,7 @@
       __extends(StepView, _super);
 
       function StepView() {
+        this.clicked = __bind(this.clicked, this);
         this.render = __bind(this.render, this);
         _ref3 = StepView.__super__.constructor.apply(this, arguments);
         return _ref3;
@@ -130,10 +135,14 @@
         'data-collapsed': 'true'
       };
 
+      StepView.prototype.events = {
+        "click .submit": "clicked"
+      };
+
       StepView.prototype.initialize = function() {
         var temp;
         _.bindAll(this);
-        temp = "\n<h3>\n<%= title %> \n</h3>\n\n<div style=\"font-size: 14px; color: #333; background-color: pink; padding: 10px; border-radius: 10px\"> \n<%= description %>\n</div>\n\n\n<!-- add new strategy -->\n<% _.each(fields, function(field) { %> \n	<input name=\"\" class=\"textinput\" placeholder=\"Add <%= field %>\" value=\"\" type=\"text\" data-mini=\"false\" />\n<% }); %>\n\n<input type=\"submit\" value=\"Submit\" />\n\n\n\n\n<!--display old strategies -->\n<% _.each(strategies, function(strat) { %> <li><%= strat %></li> <% }); %>\n\n";
+        temp = "\n<h3>\n<%= step_num %> : <%= title %>\n</h3>\n\n<div style=\"font-size: 14px; color: #333; background-color: pink; padding: 10px; border-radius: 10px\"> \n<%= description %>\n</div>\n\n\n<!-- add new strategy -->\n<% _.each(fields, function(field) { %> \n	<input name=\"\" class=\"textinput\" placeholder=\"Add <%= field %>\" value=\"\" type=\"text\" data-mini=\"false\" />\n<% }); %>\n\n<input id=\"<%= step_num %>\" class=\"submit\" type=\"submit\" value=\"Submit\" />\n\n\n\n\n<!--display old strategies -->\n<% _.each(strategies, function(strat) { %> <li><%= strat %></li> <% }); %>\n\n";
         return this.template = _.template(temp);
       };
 
@@ -142,6 +151,15 @@
         content = this.template(this.model.toJSON());
         $(this.el).html(content);
         return this;
+      };
+
+      StepView.prototype.clicked = function(e) {
+        var output;
+        e.preventDefault();
+        output = [];
+        return $(this.el).find('.textinput').each(function() {
+          return output.push($(this).val());
+        });
       };
 
       return StepView;
@@ -156,7 +174,8 @@
       }
 
       Step.prototype.defaults = {
-        title: 'Step 0',
+        step_num: 0,
+        title: '',
         description: 'Write some strategies.',
         fields: [],
         strategies: []
@@ -165,21 +184,40 @@
       return Step;
 
     })(Backbone.Model);
-    new MyRouter;
-    Backbone.history.start();
-    return $(document).on("click", "a:not([data-bypass])", function(evt) {
-      var href, root;
-      href = {
-        prop: $(this).prop("href"),
-        attr: $(this).attr("href")
-      };
-      root = location.protocol + "//" + location.host + "/";
-      if (href.prop && href.prop.slice(0, root.length) === root) {
-        evt.preventDefault();
-        Backbone.history.navigate(href.attr, true);
+    window.App = (function(_super) {
+      __extends(App, _super);
+
+      function App() {
+        _ref5 = App.__super__.constructor.apply(this, arguments);
+        return _ref5;
       }
-      return $('#myPanel').panel("close");
-    });
+
+      App.prototype.initialize = function() {
+        new MyRouter;
+        Backbone.history.start();
+        return this.initMenu();
+      };
+
+      App.prototype.initMenu = function() {
+        return $(document).on("click", "a:not([data-bypass])", function(evt) {
+          var href, root;
+          href = {
+            prop: $(this).prop("href"),
+            attr: $(this).attr("href")
+          };
+          root = location.protocol + "//" + location.host + "/";
+          if (href.prop && href.prop.slice(0, root.length) === root) {
+            evt.preventDefault();
+            Backbone.history.navigate(href.attr, true);
+          }
+          return $('#myPanel').panel("close");
+        });
+      };
+
+      return App;
+
+    })(Backbone.Model);
+    return app = new App;
   });
 
 }).call(this);
